b7562d146187aca70cf43935eaf1dfd0a5c50b0a
Yang Haitao <583364851@qq.com>
2020-01-23 17:11:59 +0800

    增加函数判断

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,9 @@
 // 实现斐波那契
 export const cb = function(s: string): string {
-    return s;
+  return s.toString();
+};
+
+//判断是否大于10
+export const isHasNumber = function(list: Array<number>): boolean {
+  return list.some(v => v > 10);
 };
