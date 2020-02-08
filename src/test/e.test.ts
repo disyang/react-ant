db53d09fc76de68fa512ee4090744030503b847a
Yang Haitao <583364851@qq.com>
2020-02-08 15:32:54 +0800

    react测试

--- src/test/e.test.ts
+++ src/test/e.test.ts
@@ -1,28 +1,24 @@
 import { sum } from '../utils';
 
 afterEach(() => {
-  console.log(4444);
 });
 
 describe('测试', () => {
   test('测试1', () => {
-    console.log(111);
     expect(sum(1, 2)).toBe(3);
   });
   test('测试2', () => {
-    console.log(2222);
     expect(sum(1, 2.5)).toBe(3.5);
   });
-  console.log(3333);
 });
 
 describe.each([
   [1, 2, 3],
-  [1, 21, 23],
+  [1, 21, 22],
   [10, 2, 12],
   [11, 22, 33],
   [105, 2, 107],
-  [100, 2, 103]
+  [100, 2, 102]
 ])('name %i + %i', (a, b, expected) => {
   test(`return${expected}`, () => {
     expect(sum(a, b)).toBe(expected);
